558f60a02b60609499aa22af65208059cd440929
unknown <55892093+Nikita-2007@users.noreply.github.com>
2021-08-20 17:59:04 +0300

    вёрска

--- JS/Script.js
+++ JS/Script.js
@@ -1,17 +1,25 @@
-var nt = "img/notepad_plus_plus.png", bat = "img/Bat.png"
+var nt = "img/notepad_plus_plus.png", bat = "img/Bat.png", cpp = "img/notepad_plus_plus.png", vs = "img/notepad_plus_plus.png",
+js = "img/notepad_plus_plus.png", java = "img/notepad_plus_plus.png", as = "img/AndroidStodio.png",
+csh = "img/notepad_plus_plus.png", py = "img/python.png", qt = "Qt.png", pas = "img/notepad_plus_plus.png";
 var listProg = [
     ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
-    ["Hello World!", "2018", "Моя самая первая программа", "img/game1.png", nt, bat],
+    ["Hello World! 2", "2018", "Тот же самое но подругому", "img/game2.png", nt, bat],
+    ["Кам-нож-бум", "2018", "Первая моя игра", "img/game3.png", nt, bat],
+    ["Пароль", "2018", "Нужно ввести верный пароль", "img/game4.png", nt, bat],
+    ["Сартировка пузырьком", "2019", "Моя первая сортировка", "img/game5.png", cpp, vs],
+    ["Угадай число", "2018", "Больше-меньше", "img/game6.png", nt, bat],
+    ["Анимация", "2018", "Спрайт", "img/game7.png", nt, bat],
+    ["Таблица умножения", "2018", "Без коментариев", "img/game8.png", cpp, vs],
+    ["Угадай число на С++", "2018", "Больше-меньше", "img/game9.png", cpp, vs],
+    ["Змейка", "2019", "Собирай яблоки что бы выжыть", "img/game10.png", nt, js],
+    ["Тетрис", "2019", "Обычный класический тетрис", "img/game11.png", nt, bat],
+    ["SpaceZombie", "2019", "Моя  первая игра на телефон", "img/game12.jpg", nt, bat],
+    ["Кам-нож-бум мобайл", "2019", "Камень ножницы бумага на телефон", "img/game13.jpg", nt, bat],
+    ["Osu! на минималках", "2020", "Жалкая породия осу", "img/game14.jpg", py, vs],
+    ["Froctal", "2020", "Бесконечные рисункии", "img/game15.jpg", py, vs],
+    ["Аквариум", "2020", "Сдесь плавают рыбки", "img/game16.jpg", py, vs],
+    ["Танчики", "2020", "Война танков", "img/game17.jpg", py, vs],
+    ["WebInfo", "2020", "Помогает выполнять различные операции", "img/game18.jpg", py, qt],
 ];
 
 var section = document.getElementById("programs");
